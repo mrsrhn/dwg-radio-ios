507a0e597fdd1ac26e08bfbe1969e07f9eda3610
Marius Rhein <inbox@mariusrhein.de>
2023-07-04 19:39:10 +0200

    fix: fixed linter errors

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
-module.exports = function (api) {
+function babelConfig(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
-};
+}
+
+module.exports = babelConfig;
--- src/DWGBottomSheet.tsx
+++ src/DWGBottomSheet.tsx
@@ -1,13 +1,13 @@
-import BottomSheet from "@gorhom/bottom-sheet";
-import { View, Text } from "react-native";
-import React, { useCallback, useMemo, useRef } from "react";
+import BottomSheet from '@gorhom/bottom-sheet';
+import { View, Text } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
 
-export const DWGBottomSheet = () => {
+function DWGBottomSheet() {
   const bottomSheetRef = useRef<BottomSheet>(null);
-  const snapPoints = useMemo(() => ["60%", "90%"], []);
+  const snapPoints = useMemo(() => ['60%', '90%'], []);
 
   const handleSheetChanges = useCallback((index: number) => {
-    console.log("handleSheetChanges", index);
+    console.log('handleSheetChanges', index);
   }, []);
 
   return (
@@ -23,4 +23,6 @@ export const DWGBottomSheet = () => {
       </View>
     </BottomSheet>
   );
-};
+}
+
+export default DWGBottomSheet;
