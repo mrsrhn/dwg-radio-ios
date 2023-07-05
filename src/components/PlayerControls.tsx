97e9182ec1f247feb4eb7b4ca3cbf69769fab33e
Marius Rhein <inbox@mariusrhein.de>
2023-07-05 14:49:34 +0200

    feat: implement basic radio player functionality

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,15 +1,19 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import useStores from '../hooks/useStores';
 
 const PlayerControls = observer(() => {
   const { playerStore } = useStores();
+
+  const onPlayButtonPress = () => playerStore.togglePlayer();
+
   return (
     <View style={{ flex: 1 }}>
+      <Text>{playerStore.title}</Text>
       <Button
-        title={JSON.stringify(playerStore.isPlaying)}
-        onPress={() => playerStore.setIsPlaying(!playerStore.isPlaying)}
+        title={playerStore.isPlaying ? 'Pause' : 'Play'}
+        onPress={onPlayButtonPress}
       />
     </View>
   );
--- src/stores/playerStore.ts
+++ src/stores/playerStore.ts
@@ -1,17 +1,79 @@
 import { action, makeObservable, observable } from 'mobx';
+import {
+  Audio,
+  AVMetadata,
+  AVPlaybackStatusError,
+  AVPlaybackStatusSuccess,
+} from 'expo-av';
+import { Config } from '../types/config';
 
 class PlayerStore {
+  isLoaded = false;
+
   isPlaying = false;
 
-  constructor() {
+  player: Audio.Sound | undefined = undefined;
+
+  title = '';
+
+  config: Config;
+
+  constructor(config: Config) {
     makeObservable(this, {
       isPlaying: observable,
+      title: observable,
     });
+    this.config = config;
+    this.initAudioPlayer();
   }
 
   setIsPlaying = action((isPlaying: boolean) => {
     this.isPlaying = isPlaying;
   });
+
+  setIsLoaded = action((isLoaded: boolean) => {
+    this.isLoaded = isLoaded;
+  });
+
+  setTitle = action((title: string) => {
+    this.title = title;
+  });
+
+  togglePlayer = () => {
+    if (!this.player) return;
+    if (this.isPlaying) {
+      this.player.pauseAsync();
+    } else {
+      this.player.playAsync();
+    }
+  };
+
+  initAudioPlayer = async () => {
+    const { sound: playbackObject } = await Audio.Sound.createAsync(
+      { uri: this.config.configBase.url },
+      {},
+      this.onPlaybackStatusUpdate
+    );
+    playbackObject.setOnMetadataUpdate(this.onMetadataUpdate);
+
+    this.player = playbackObject;
+  };
+
+  private onPlaybackStatusUpdate = (
+    status: AVPlaybackStatusError | AVPlaybackStatusSuccess
+  ) => {
+    this.setIsLoaded(status.isLoaded);
+    if ('isPlaying' in status) {
+      this.setIsPlaying(status.isPlaying);
+    } else if (status.error) {
+      console.error(status.error);
+      this.setIsPlaying(false);
+    }
+  };
+
+  private onMetadataUpdate = (metadata: AVMetadata) => {
+    this.setTitle(metadata.title ?? '');
+  };
 }
 
 export default PlayerStore;
