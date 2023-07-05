fd44415c9c0a5909dd62e9218c8e3270aca841c0
Marius Rhein <inbox@mariusrhein.de>
2023-07-05 14:49:34 +0200

    feat: implement basic radio player functionality

--- src/stores/rootStore.ts
+++ src/stores/rootStore.ts
@@ -1,10 +1,14 @@
+import { Config } from '../types/config';
 import PlayerStore from './playerStore';
+import configBase from '../config/configBase.json';
 
 class RootStore {
   playerStore: PlayerStore;
 
+  config: Config = { configBase };
+
   constructor() {
-    this.playerStore = new PlayerStore();
+    this.playerStore = new PlayerStore(this.config);
   }
 }
 
