acdeca6d121f8fb01a0095c11c40855ab1324c81
Marius Rhein <inbox@mariusrhein.de>
2023-07-17 16:09:55 +0200

    chore: remove console log

--- src/components/DWGBottomSheet.tsx
+++ src/components/DWGBottomSheet.tsx
@@ -3,7 +3,7 @@ import BottomSheet, {
   useBottomSheetDynamicSnapPoints,
 } from '@gorhom/bottom-sheet';
 import { observer } from 'mobx-react-lite';
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import { StyleSheet } from 'react-native';
 import HistoryView from './HistoryView';
 import PlayerControls from './PlayerControls';
@@ -20,15 +20,10 @@ const DWGBottomSheet = observer(() => {
     handleContentLayout,
   } = useBottomSheetDynamicSnapPoints(initialSnapPoints);
 
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
-
   return (
     <BottomSheet
       ref={bottomSheetRef}
       index={0}
-      onChange={handleSheetChanges}
       animateOnMount={false}
       snapPoints={animatedSnapPoints}
       handleHeight={animatedHandleHeight}
