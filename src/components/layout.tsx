35582c63ba69ffe83e2e65b89785bfbc96f5abd0
Marius Rhein <inbox@mariusrhein.de>
2023-07-19 14:56:05 +0200

    feat: fix status bar text color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react';
 import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
 import Colors from '../Colors';
 import DWGBottomSheet from './DWGBottomSheet';
 import DWGPager from './DWGPager';
@@ -7,6 +10,7 @@ import DWGPager from './DWGPager';
 function Layout() {
   return (
     <>
+      <StatusBar style="dark" />
       <View style={{ flex: 1 }}>
         <DWGPager />
       </View>
