8e214e0cb83feab4d17c028bfed7837ce354d0a7
paschsaar <paul.schitz@dwgradio.de>
2024-10-22 20:23:28 +0200

    fix error during init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import TrackPlayer from 'react-native-track-player';
 import App from './App.tsx';
 
-TrackPlayer.registerPlaybackService(() => {
+TrackPlayer.registerPlaybackService(() => async () => { 
   TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
   TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
 });
